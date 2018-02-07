80d172a1c802afb2db7d4a9e48fed99be81cc556
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-07 18:51:55 -0500

    Functional multicounter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      value: 0
+    }
+    this.add1 = this.add1.bind(this);
+    this.add2 = this.add2.bind(this);
+    this.add3 = this.add3.bind(this);
+  }
+  add1(){
+    this.setState({count: this.state.count + 1});
+  }
+  add2(){
+    this.setState({count: this.state.count + 2});
+  }
+  add3(){
+    this.setState({count: this.state.count + 3});
+  }
+  valueChange(e){
+    this.setState({value: +e.target.value});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input type="number" value={this.state.count}/>
+        <br/>
+        <button value="1" onClick={this.add1}>1</button>
+        <button value="2" onClick={this.add2}>2</button>
+        <button value="3" onClick={this.add3}>3</button>
+        <br/>
+        <button onClick={() => this.setState({count: 0})}>RESET</button>
       </div>
     );
   }
